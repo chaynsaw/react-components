75ef0cdd7f040f0757ce0c7aac1742aa2c3ba182
chaynsaw <chaynsaw@gmail.com>
2019-04-22 16:39:02 -0700

    Completed components

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// TODO
+let items = ['milk', 'cheese', 'juice'];
+
+let GroceryList = () => {
+	return (<ul> 
+	{items.map((item) => {
+		return <GroceryListItem item={item}/> 
+	})}
+	</ul>)
+}
+
+
+// let GroceryListItem = (props) => {
+// 	console.log(props)
+// 	return <li>{props.item}</li>
+// }
+
+class GroceryListItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			done: false 
+		};
+	}
+	onListItemClick() {
+		this.setState({
+			done: !this.state.done
+		})
+	}
+	render() {
+		let style = {
+			textDecoration: this.state.done ? 'line-through': 'none'
+		};
+		return (
+			<li style= {style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+		);
+	}
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
